1a5479008b87169b1104df5879b776a1ca06bf0c
firman742 <hafidzfirman742@gmail.com>
2024-09-07 21:26:37 +0700

    Create Connection to Bluetooth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { CommonModule } from "@angular/common";
+import { MuseClient } from 'muse-js';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    MatSlideToggleModule
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-eeg-project';
+  eegData: number[] = [];
+  museClient: MuseClient;
+
+  constructor() {
+    this.museClient = new MuseClient();
+  }
+
+  async connectMuse(): Promise<void> {
+    try {
+      // Connect to the Muse device
+      await this.museClient.connect();
+      await this.museClient.start();
+
+      console.log('Connected to Muse!');
+
+      // Subscribe to EEG data streams
+      this.museClient.eegReadings.subscribe(reading => {
+        // Flattening the data from multiple channels
+        const data = reading.samples.map(sample => sample).flat();
+        console.log('EEG Data:', data);
+
+        // Store the data in an array to display in the template
+        this.eegData = data;
+      });
+    } catch (error) {
+      console.error('Error connecting to Muse:', error);
+    }
+  }
 }
